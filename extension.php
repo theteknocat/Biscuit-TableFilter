1fdeca5fbd455766c573a2feb7443ffcdb9346c0
Peter Epp <peter@teknocat.org>
2013-04-11 21:16:44 -0600

    Updated to Biscuit 2.2 version

--- extension.php
+++ extension.php
@@ -3,10 +3,11 @@
  * Extension for easy inclusion of Table Filter JS file
  *
  * @package Extensions
+ * @subpackage TableFilter
  * @author Peter Epp
  * @copyright Copyright (c) 2009 Peter Epp (http://teknocat.org)
  * @license GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.html)
- * @version 2.0
+ * @version 2.0 $Id: extension.php 13982 2011-08-08 18:51:55Z teknocat $
  **/
 class TableFilterJs extends AbstractExtension {
 	function run() {
